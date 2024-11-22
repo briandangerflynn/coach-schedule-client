48f28255d4d2e1f7b30e976d5c52bec508bf75a1
Brian Flynn <brianflynn@brians-mbp.mynetworksettings.com>
2024-11-22 09:06:44 -0500

    small refactor

--- src/components/appointments/ActionButtons.jsx
+++ src/components/appointments/ActionButtons.jsx
@@ -1,15 +1,19 @@
 import { useAuth } from "../../context/AuthContext";
 import StudentButtons from "./StudentButtons";
 import CoachButtons from "./CoachButtons";
+import { appointmentComplete } from "./utils";
 
 export default function ActionButtons({appointment, setAppointments}) {
   const { user } = useAuth();
+  const bookable = user?.role === "student" && 
+    appointment.student_id === null && 
+    !appointmentComplete(appointment.end_time);
 
   if(user?.role === "coach"){
     return(
       <CoachButtons appointmentId={appointment.id} setAppointments={setAppointments} />
     ) 
-  } else if(user?.role === "student" && appointment.student_id === null){
+  } else if(bookable){
     return (
       <StudentButtons appointmentId={appointment.id} setAppointments={setAppointments} />
     )
